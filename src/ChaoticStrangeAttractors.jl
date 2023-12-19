2ea20d73f20630ae5cd8b42874adb5ce891916fc
Sagnac <83491030+Sagnac@users.noreply.github.com>
2023-12-19 20:42:59 +0000

    Overload `show` instead of `display`
    `display` (and even `print`) ultimately call `show` so it's better to
    overload that. I think I was overloading `display` before doing any
    of the custom printing; if returning nothing `show` adds an extra line
    and since the REPL invokes `display` it's more direct in a sense and
    avoids that extra line.

--- src/ChaoticStrangeAttractors.jl
+++ src/ChaoticStrangeAttractors.jl
@@ -158,13 +158,27 @@ function attract!(
     return attractors
 end
 
-function Base.display(attractor::T) where T <: Attractor
+# printing vectors will first call 3-arg show and fall back to 2-arg show for output
+# if there are line breaks; this allows the figure display logic to fire while at the
+# same time printing the more concise format
+function Base.show(io::IO, attractor::T) where T <: Attractor
+    for name ∈ fieldnames(T)
+        name == :x && break
+        @printf(io, "%s = %.4f, ", name, getfield(attractor, name))
+    end
+    @printf(io, "x_0 = %.4f, y_0 = %.4f, z_0 = %.4f", first.(attractor.points)...)
+end
+
+function Base.show(io::IO, ::MIME"text/plain", attractor::T) where T <: Attractor
     (; fig) = attractor
+    print(io, T, " attractor:")
     for name ∈ fieldnames(T)
         name == :dt && break
-        name == :x && @printf("\nx_0 = %.4f\ny_0 = %.4f\nz_0 = %.4f\nΔt = %.4f\n\n",
-            first.(attractor.points)..., attractor.dt)
-        @printf("%s = %.4f\n", name, getfield(attractor, name))
+        if name == :x
+            @printf(io, "\n\nx_0 = %.4f\ny_0 = %.4f\nz_0 = %.4f\nΔt = %.4f\n",
+                first.(attractor.points)..., attractor.dt)
+        end
+        @printf(io, "\n%s = %.4f", name, getfield(attractor, name))
     end
     (isempty(fig.content) || events(fig).window_open[]) && return
     display(GLMakie.Screen(), fig)
