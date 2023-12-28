Sagnac/ChaoticStrangeAttractors.jl

commit df0504b6269d1473cdafc9d72ec435db0588a76b

Tie up some loose ends from 2fbb145
- the pause state doesn't need to be set within `State`s inner
constructor

- the attractors no longer need to be returned as there's no vector to
AttractorSet conversion

src/ChaoticStrangeAttractors.jl
@@ -23,7 +23,6 @@ mutable struct State
     function State()
         state = new()
         state.timers = [Timer(0.0) for i = 1:2]
-        state.paused = true
         return state
     end
     function State(segments, position, axis, colors, timers, paused)
@@ -87,7 +86,7 @@ function init!(attractor::Attractor)
     for (name, value) ∈ pairs((; segments, position, colors))
         setfield!(attractor.state, name, value)
     end
-    return attractor
+    return
 end
 
 function init!(attractors::Vector{<:Attractor})
@@ -99,7 +98,7 @@ function init!(attractors::Vector{<:Attractor})
         attractor.state.timers = initial.state.timers
         init!(attractor)
     end
-    return attractors
+    return
 end
 
 function set!(attractors::Attractors)
@@ -113,9 +112,9 @@ function set!(attractors::Attractors)
         title = "$T attractor"
     end
     attractor.state.axis = Axis3(fig[1,1]; title)
-    attractors = init!(attractors)
+    init!(attractors)
     display(GLMakie.Screen(), fig)
-    return attractors, title
+    return title
 end
 
 function pause(attractor::Attractor, state::Bool = !attractor.state.paused[])
@@ -136,7 +135,7 @@ end
 
 function attract!(attractors::Attractors = Rossler();
                   t::Real = 125, paused::Bool = false)
-    attractors, = set!(attractors)
+    set!(attractors)
     attractor = attractors[]
     (; fig) = attractor
     for attractor ∈ attractors
@@ -166,7 +165,7 @@ function attract!(
     attractors :: Attractors = Aizawa();
     t          :: Real       = 125
 )
-    attractors, title = set!(attractors)
+    title = set!(attractors)
     attractor = attractors[]
     (; fig) = attractor
     itr = range(1, t / interval)

commit 21e034da2af8a8e9e3e084694cc920d6f7a7c591

`show` fixes
* df0504b caused a regression since displaying vectors requires the
  `paused` state;

* 3-arg `show` is now preferred so that 2-arg `show` can be used as a
  proper fallback (e.g. calling it directly or invoking
  `Set([attractor])`;

* `return` `nothing` for type stability;

* check the existence of the :typeinfo key instead of checking whether
  or not it's a `Type`.

src/ChaoticStrangeAttractors.jl
@@ -23,6 +23,7 @@ mutable struct State
     function State()
         state = new()
         state.timers = [Timer(0.0) for i = 1:2]
+        state.paused = true
         return state
     end
     function State(segments, position, axis, colors, timers, paused)

src/Overload.jl
@@ -23,9 +23,10 @@ function show_params(io::IO, attractor::T) where T <: Attractor
     @printf(io, "x_0 = %.4f, y_0 = %.4f, z_0 = %.4f", first(attractor.points)...)
 end
 
-function show(io::IO, attractor::Attractor)
-    f = get(io, :typeinfo, false) isa Type ? show_params : recap
+function show(io::IO, ::MIME"text/plain", attractor::Attractor)
+    f = haskey(io, :typeinfo) ? show_params : recap
     f(io, attractor)
+    return
 end
 
 function display(attractors::Vector{<:Attractor})